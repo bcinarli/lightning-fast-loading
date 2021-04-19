d5d31eeb55d8322196b5927f6e4a9ea6ff249b2a
Bilal Cinarli <bilal.cinarli@adidas.com>
2021-04-19 02:26:48 +0200

    Use next-gen images

--- src/scripts/components/product-card/product-card.tsx
+++ src/scripts/components/product-card/product-card.tsx
@@ -13,11 +13,15 @@ export type Product = {
 };
 
 const ProductCard: FC<Product> = ({ name, image, price, category }) => {
+  const nextgen = image.split('.').slice(0, -1).join('.');
+
   return (
     <div className={css('product-card')}>
       <div
         className={css('product-card-image')}
-        style={{ backgroundImage: `url(${IMAGE_API}${image})` }}
+        style={{
+          backgroundImage: `url(${IMAGE_API}/next-gen/${nextgen}.webp)`
+        }}
       />
 
       <h3 className={css('product-card-title')}>{name}</h3>
